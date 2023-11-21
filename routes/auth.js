110fad68067dd2e21c3d8dbda9735a07cb784ac0
Thejus Varma <thejusvarma11@gmail.com>
2023-11-21 09:03:04 +0530

    bcrypt implemented

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require("../models/User");
 const { query, body, matchedData, validationResult } = require('express-validator');
-
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 router.post('/createuser',body('name','Enter valid Name').isLength({min:3}),
@@ -11,17 +11,19 @@ router.post('/createuser',body('name','Enter valid Name').isLength({min:3}),
     const errors = validationResult(req);
     // if validation errors are found then it goes inside IF returns error message in response
     if (!errors.isEmpty()) {
-        // const user = User(req.body);
-        // user.save()
+
         return res.send({ errors: errors.array() });
     }
     // if no errors then proceeds to create register the data into user DB
     try {
+        const saltRounds = 10;
+        // creating hashed password using passwor + salt 
+        const secpass = await bcrypt.hash(req.body.password, saltRounds)
         const user = await User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
-        });
+                name: req.body.name,
+                email: req.body.email,
+                password: secpass
+                });
         res.json(user);
         } 
     catch (error) 
