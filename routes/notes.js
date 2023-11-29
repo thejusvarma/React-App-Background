655011118793d47c3526df7831d8670c5bd2ff30
Thejus Varma <thejusvarma11@gmail.com>
2023-11-29 15:43:50 +0530

    note deletion route added

--- routes/notes.js
+++ routes/notes.js
@@ -27,7 +27,7 @@ router.get('/fetchallnotes',fetchuser,async (req,res)=>
 })
 
 //add notes using POST "api/notes/addnotes"
-router.post('/addnotes',body('title','Title should be minimum of 3 characters').isLength({min:3}),
+router.post('/addnote',body('title','Title should be minimum of 3 characters').isLength({min:3}),
                         body('content','Content should be of atleast 5 characters').isLength({min:5}),
         fetchuser, async(req,res)=>
 {
@@ -49,6 +49,56 @@ router.post('/addnotes',body('title','Title should be minimum of 3 characters').
     {
         console.error(err.message);
         res.status(500).send("DB Error"); 
+    }
+})
+
+// Route 3 updating the existing note "api/notes/updatenote"
+router.put('/updatenote/:id',body('title','Title should be minimum of 3 characters').isLength({min:3}),
+                        body('content','Content should be of atleast 5 characters').isLength({min:5}),
+        fetchuser, async(req,res)=>
+{
+    const { title, content, tag } = req.body;
+    try {
+        // Create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (content) { newNote.content = content };
+        if (tag) { newNote.tag = tag };
+
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+// Route 3 updating the existing note "api/notes/updatenote"
+router.delete('/deletenote/:id',
+        fetchuser, async(req,res)=>
+{
+
+    try {
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json("Deleted Successfully")
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
     }
 })
 
